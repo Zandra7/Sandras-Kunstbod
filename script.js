e239f9505f4ac68c00b9d13ac0a9b1a2df128f77
Sandra Elise Eng <sandra.eng@live.no>
2023-02-08 17:33:05 +0100

    Lagt til tilfeldig bilde

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ let penger = 0
 function lagBilder() {
     bilder ++;
     document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder + ' $'
+    fetch("https://source.unsplash.com/random")
+        .then(function(response) {
+            return response.url;
+        })
+        .then(function(imageUrl) {
+            document.getElementById("tilfeldigBilde").src = imageUrl;
+            tilfeldigRotasjon()
+        });
+
 }
 
 function selgBilder() {
@@ -11,4 +20,9 @@ function selgBilder() {
     document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $'
     bilder -= bilder
     document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder + ' $'
+}
+
+function tilfeldigRotasjon() {
+    const randomDegree = Math.floor(Math.random() * 180);
+    document.getElementById("tilfeldigBilde").style.transform = `rotate(${randomDegree}deg)`;
 }
