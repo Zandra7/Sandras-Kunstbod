7612bf7fbcc3069d1c80ca1a79b75846af46be52
Sandra Elise Eng <sandra.eng@live.no>
2023-02-08 17:02:21 +0100

    Selge bilder

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let bilder = 0
+let penger = 0
 
 function lagBilder() {
     bilder ++;
-    document.getElementById('lagBilde').innerHTML = 'Bilder på lager: ' + bilder
+    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder + ' $'
+}
+
+function selgBilder() {
+    penger += bilder
+    document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $'
+    bilder -= bilder
+    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder + ' $'
 }
