ee6bd52bdee06dbae391fdb9a6bec211e219a2d8
Sandra Elise Eng <sandra.eng@live.no>
2023-02-08 20:17:52 +0100

    Ansette folk

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ let bilderPerKlikk = 1
 let penger = 0
 
 let OppgraderUtstyrPris = 40
+let AnsettePris = 100
 
 function selgBilder() {
     penger += bilder
     document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $'
     bilder -= bilder
-    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder + ' $'
+    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder
 }
 
 function lagBilder() {
     bilder += bilderPerKlikk;
-    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder + ' $'
+    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder
     fetch("https://source.unsplash.com/random")
         .then(function(response) {
             return response.url;
@@ -31,6 +32,20 @@ function OppgraderUtstyr() {
         OppgraderUtstyrPris *= 2
         document.getElementById('OppgraderUtstyrPris').innerHTML = 'Pris: ' + OppgraderUtstyrPris + ' $'
         bilderPerKlikk += 1
+    }
+}
+
+function AnsetteFolk() {
+    if (penger >= AnsettePris) {
+        penger -= AnsettePris
+        document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $'
+
+        setInterval(function() {
+            bilder += 1;
+            document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder
+        }, 1500);
+        AnsettePris *= 2
+        document.getElementById('AnsettePris').innerHTML = 'Pris: ' + AnsettePris + ' $'
     }
 }
 
