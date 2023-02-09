7159ed0ccf632700189928cd666f094be5a88410
Sandra Elise Eng <sandra.eng@live.no>
2023-02-09 10:28:19 +0100

    Kommentert noe kode

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// ------ VARIABLER ------ //
+
 let bilder = 0
 let bilderPerKlikk = 1
 let penger = 0
@@ -5,33 +8,38 @@ let penger = 0
 let OppgraderUtstyrPris = 40
 let AnsettePris = 100
 
+
+// ------ FUNKSJONER ------ //
+
+// 'Selg bilder knapp' //
 function selgBilder() {
-    penger += bilder
-    document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $'
-    bilder -= bilder
-    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder
+    penger += bilder // Legger til antall bilder til penger
+    document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $' // Oppdaterer tekst med id 'AntallPenger'
+    bilder -= bilder // Fjerner alle bildene
+    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder // Oppdatrerer tekst med id 'BilderPåLager'
 }
 
+// 'Lag bilder' knapp
 function lagBilder() {
-    bilder += bilderPerKlikk;
-    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder
-    fetch("https://source.unsplash.com/random")
+    bilder += bilderPerKlikk; // Legger til antall bilderPerKlikk (variabelen) til bilder
+    document.getElementById('BilderPåLager').innerHTML = 'Bilder på lager: ' + bilder // Oppdater tekst med id 'BilderPåLager'
+    fetch("https://source.unsplash.com/random") // Henter tilfeldig bilde fra Unsplash
         .then(function(response) {
             return response.url;
         })
         .then(function(imageUrl) {
-            document.getElementById("tilfeldigBilde").src = imageUrl;
-            tilfeldigRotasjon()
+            document.getElementById("tilfeldigBilde").src = imageUrl; // Oppdaterer bildet med id 'tilfeldigBilde'
+            tilfeldigRotasjon() // Kjører funksjon som roterer bildet med tilfeldig tall
         });
 }
-
+// 'Oppgrader utstyr' knapp
 function OppgraderUtstyr() {
-    if (penger >= OppgraderUtstyrPris) {
-        penger -= OppgraderUtstyrPris
-        document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $'
-        OppgraderUtstyrPris *= 2
-        document.getElementById('OppgraderUtstyrPris').innerHTML = 'Pris: ' + OppgraderUtstyrPris + ' $'
-        bilderPerKlikk += 1
+    if (penger >= OppgraderUtstyrPris) { // Hvis penger er høyere enn OppgraderUtstyrPris (variabelen)
+        penger -= OppgraderUtstyrPris // Fjern tallet OppgraderUtstyrPris (variabelen) fra penger (variabelen)
+        document.getElementById('AntallPenger').innerHTML = 'Penger: ' + penger + ' $' // Oppgrader teksten med id 'AntallPenger'
+        OppgraderUtstyrPris *= 2 // Øker prisen med * 2
+        document.getElementById('OppgraderUtstyrPris').innerHTML = 'Pris: ' + OppgraderUtstyrPris + ' $' // Oppdaterer teksten med id 'OppgraderUtstyrPris
+        bilderPerKlikk += 1 // Legger til 1 hver gang du klikker
     }
 }
 
